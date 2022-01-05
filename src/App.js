7014e59e77845778efa3d29b8cacd117f7763175
hirediversity <official@hirediversity.club>
2022-01-05 10:22:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Content from './components/Content'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyJzEKvIACHqssJQ'}).base('appmdn5kLtTSnivLC');
+
+const table = base('통합데이터관리');
+const data = [];
+
+const getRecords = async () => {
+  const records = await table.select({
+      maxRecords: 99999,
+      view: 'APP용'
+  }).eachPage((lists, fetchNextPage) => {
+    lists.forEach((list) => data.push(list.fields))
+    fetchNextPage();
+  });
 }
 
-export default App;
+getRecords();
+
+
+function App() {
+
+  const [box, setBox] = useState(false)
+
+  // setTimeout(() => {
+  //   console.log(data);
+  // }, 10000);
+
+  return (
+    <div className="App">
+      <div id="search">
+        <input id="email" placeholder='e-mail'></input>
+        <input id="rcNumber" placeholder='password'></input>
+        <button type="button" className="btn btn-primary btn-sm" id="searchBtn" onClick={() => {
+          setBox(true);
+          }}>Login</button>
+       
+          <div id="loading" className="spinner-border spinner-border-sm" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+     
+        <p id="err">* 가입 정보가 없거나, 입력하신 정보가 틀렸습니다. *</p>
+      </div>
+      <p></p>
+      <div className="container">
+          {
+            box === true
+            ? <Content 
+            data={data}
+            pw={document.getElementById('rcNumber').value}
+            em={document.getElementById('email').value}
+            inputs={document.getElementById('search')}
+            err={document.getElementById('err')}
+            loading={document.getElementById('loading')}
+            />
+            : null
+          }
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Content.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { useState } from 'react';
+import '../App.css';
+
+const Content = ({data, em, pw, inputs, err, loading}) => {
+
+    let [link, setLink] = useState(false);
+    let [open, setOpen] = useState(false);
+
+
+    if (em.length !== 0 || pw.length !== 0) {
+        loading.style.display = 'flex';
+
+        setTimeout(() => {
+            loading.style.display = 'none';
+            inputs.style.display = 'none';
+            
+            setOpen(true)
+    
+          }, 10000);
+    } 
+    else {
+        alert('이메일 또는 패스워드를 입력해주세요')
+        window.location.reload();
+    }
+    
+
+    
+
+
+      function Normal() {
+
+        for (var i = 0; i < data.length; i++) {
+
+            if (em === data[i].이메일 && pw === data[i].AppPW) {
+                inputs.style.display = 'none';
+                err.style.display = 'none';
+
+                return (
+                <div>
+                    <p>{data[i].이름}님, 안녕하세요!</p>
+                    <p>이번 달 보험료는 {data[i].수수료계좌}원,<br/>
+                    지난 달 연체료는 {data[i].수수료미납계좌}원으로<br/>
+                    총 보험료는 <b>{data[i].수수료계좌총액}원</b>입니다.</p>
+                    <p>* 아래 버튼을 클릭하여 보험료를 납부해주세요 *</p>
+                    <button onClick={() => {setLink(true)}}>돈 내세요</button>
+
+                    {
+                        link === true
+                        ? <Form />
+                        : null
+                    }
+
+                </div>
+                )
+            } else retry()
+        }
+
+        return <div></div>
+            
+
+        function retry() {
+            inputs.style.display = 'block';
+            loading.style.display = 'none';
+            err.style.display = 'block';
+            
+            // setTimeout(() => {
+            //     window.location.reload();
+            // }, 500);
+        }
+
+        function Form() {
+            window.location.href = `${data[i].안내용개별링크}`
+        }
+        
+      }
+
+      return (
+        <div>
+            {
+                open === true
+                ? <Normal />
+                : null
+            }
+        </div>
+    )
+}
+export default Content
