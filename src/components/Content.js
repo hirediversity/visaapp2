b0ef6526d14e9f4170620a43dd1667a032e5a570
hirediversity <96763424+hirediversity@users.noreply.github.com>
2022-03-07 18:12:03 +0900

    중국어 추가

--- src/components/Content.js
+++ src/components/Content.js
@@ -37,14 +37,38 @@ const Content = ({data, em, pw, inputs, loading, logo}) => {
                 logo.style.display = 'none';
 
                 let 진행상황 = data[i].진행상황.replace(/\.|\-|[(0-9)]/g,'');
+                let 진행중국어 = 진행상황.replace('사법처리대상', '罚款处理对象')
+                                        .replace('심사대기', '材料审核待机')
+                                        .replace('검토중', '材料审核中')
+                                        .replace('보완서류 요청', '材料补交请求中')
+                                        .replace('서류 준비 완료', '材料准备完成')
+                                        .replace('서류 출력 완료', '材料打印完成')
+                                        .replace('서류 접수 완료', '材料已提交至出入境事务所')
+                                        .replace('반려', '材料已驳回')
+                                        .replace('반려email안내 완료', '材料驳回通知邮件已发送')
+                                        .replace('반려서류 준비 완료', '驳回材料提交准备完成')
+                                        .replace('지문등록 안내', '指纹登陆已通知')
+                                        .replace('지문등록 완료', '指纹登陆已完成')
+                                        .replace('외국인등록증 수령', '外国人登陆证已领取')
+                                        .replace('외국인등록증 배송시작', '外国人登陆证已开始配送')
+                                        .replace('외국인등록증 배송시작 email안내 완료', '外国人登陆证配送通知邮件发送完成')
+                                        .replace('외국인등록증 배포 완료', '外国人登陆证分发完毕')
+                                        .replace('절차 모두 완료', '所有步骤均已完成')
+                                        .replace('무효신청', '无效申请')
+
 
                 return (
                 <div className='조회화면'>
                     <p>({data[i].하다시연번})<br/>
                     {data[i].전체이름}님,<br/>
                     안녕하세요!</p>
-                    <p>현재 {data[i].차수} 외국인등록증 신청 진행 상황은<br/>
-                    <b>{진행상황}</b>입니다.</p>
+                    <p>현재 외국인등록증 신청 진행 상황은<br/>
+                    <b>{진행상황}</b>입니다.</p><br/>
+
+                    <p>{data[i].전체이름}同学<br/>
+                    您好</p>
+                    <p>目前国人登陆证申请进行状态为<br/>
+                    <b>{진행중국어}</b></p><br/>
 
                     {
 
@@ -100,8 +124,9 @@ const Content = ({data, em, pw, inputs, loading, logo}) => {
 
             return (
                 <>
-                    <p>보완 사유는 아래와 같습니다.<br/>
-                    <span id="기간안내">(보완 마감일: {data[i].보완마감일})</span></p>
+                    <p>보완 사유는 아래와 같습니다 / 需要补交的材料如下<br/>
+                    <span id="기간안내">(보완 마감일 / 材料补交截止日期 : {data[i].보완마감일})</span></p>
+
                     <div id="reasonBox">{newRea}</div>
 
                     <button id="납부하기" onClick={() => {setLink2(true)}}>보완서류 제출 / 点击补交材料<br/>Submission of supplementary documents</button>
