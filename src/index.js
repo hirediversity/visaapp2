3da831526bfa23dd7526550eb9ca04dfb88bed06
hirediversity <96763424+hirediversity@users.noreply.github.com>
2022-07-27 16:30:58 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import KOApp from './KOApp';
+
 import reportWebVitals from './reportWebVitals';
-import CNApp from './CNApp'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,21 +12,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <KOApp />
-//   </React.StrictMode>,
-//   document.getElementById('KOroot')
-// );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <CNApp />
-//   </React.StrictMode>,
-//   document.getElementById('CNroot')
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
