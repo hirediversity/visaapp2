a5cfd751c43a348e4f1cb11f317394f0e0af18ee
hirediversity <96763424+hirediversity@users.noreply.github.com>
2022-07-27 16:23:13 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import KOApp from './KOApp';
 import reportWebVitals from './reportWebVitals';
+import CNApp from './CNApp'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +12,20 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <KOApp />
+//   </React.StrictMode>,
+//   document.getElementById('KOroot')
+// );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <CNApp />
+//   </React.StrictMode>,
+//   document.getElementById('CNroot')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
