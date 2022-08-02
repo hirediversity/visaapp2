0b14c72e6ec1ecb63e36d721d0d19c65695ce5bf
hirediversity <96763424+hirediversity@users.noreply.github.com>
2022-08-02 15:23:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -37,9 +37,7 @@ function App() {
           setBox(true);
           }}>Login</button>
 
-          <p id='loginform'>로그인이 안 되는 경우 페이지 새로고침을 하지 않고 5분 뒤에 다시 시도해 주세요<br/>
-          If you can't log in please try again 5 minutes later, do not refresh the page.<br/>
-          如果无法进行登录，请将本页面维持5分钟之后再次输入账号密码进行尝试。谢谢
+          <p id='loginform'>If you can't log in please try again 5 minutes later, do not refresh the page.
           </p>
        
           <div id="loading" className="spinner-border spinner-border-sm" role="status">
